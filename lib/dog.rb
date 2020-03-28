9a5e9f9d95459d5c972a2ac9240457969b606185
Charles Pelton <charliejp0311@gmail.com>
2020-03-28 20:48:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -55,7 +55,7 @@ class Dog
   def self.find_or_create_by(name:, breed:)
     dug = DB[:conn].execute("SELECT * FROM dogs WHERE name = ?, breed = ?", name, breed)
     if !dug.empty?
-      doggie = self.new_from_db(dug[0])
+      doggie = Dog.new_from_db(dug[0])
     else
       
     end
