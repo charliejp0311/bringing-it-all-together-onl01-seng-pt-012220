0d447fe25e2e4aaa6974b325bc016efd2943029e
Charles Pelton <charliejp0311@gmail.com>
2020-03-28 19:35:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -27,6 +27,6 @@ class Dog
         VALUES (?,?)
       SQL
       DB[:conn].execute(sql, self.name, self.breed)
-      @id = DB[:conn].execute("SELECT last_insert_rowid()")
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs;")
   end
 end
